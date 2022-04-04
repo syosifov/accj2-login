839802cb4b372dc653cc9ad50a996603f0727176
Strashimir Yosifov <syosifov@gmail.com>
2022-04-04 12:02:02 +0300

    Initial project setup

---
+++ src/main/java/net/yosifov/accounting/accj/accj2login/Accj2LoginApplication.java
@@ -0,0 +1,13 @@
+package net.yosifov.accounting.accj.accj2login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accj2LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accj2LoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/yosifov/accounting/accj/accj2login/Accj2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yosifov.accounting.accj.accj2login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Accj2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
