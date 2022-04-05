fa19f27a3eed12131a42b66e6e29c285b2b2283a
Strashimir Yosifov <syosifov@gmail.com>
2022-04-05 11:52:27 +0300

    Security checks dropeed

---
+++ src/main/java/net/yosifov/accounting/accj/accj2login/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package net.yosifov.accounting.accj.accj2login.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll();
+
+    }
+
+
+
+
+
+
+}
